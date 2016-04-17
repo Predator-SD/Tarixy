25d035aa27a42e8d05f2e6c8abc847b20249fd35
SD <zswdzly@outlook.com>
2016-04-17 12:36:42 +0800

    get type method added

--- compiler.py
+++ compiler.py
@@ -1,6 +1,6 @@
 class compiler(object):
   def __init__(self):
-    print(self.load('test.ty'))
+    print(self.gettype("   var double 233"))
   def de(self,str):
     p1=str.replace(' ','7')
     time=0
@@ -16,4 +16,18 @@ class compiler(object):
     finally:
       raw.close()
     return txt.split('\n')
+  def gettype(self,str):
+    raw=(self.de(str)).replace("char","1").replace("int","2").replace("double","3").replace("method","4")
+    type=raw[4]
+    if type=="1":
+      type="char"
+    elif type=="2":
+      type="int"
+    elif type=="3":
+      type="double"
+    elif type=="4":
+      type="method"
+    else:
+      type="error"
+    return type
 test=compiler()
